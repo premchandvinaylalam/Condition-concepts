2583dcc109fe1c42b2b14e09d485e735a480c0f4
PremChand <lpremchandvinay@gmail.com>
2025-10-22 22:20:34 +0530

    pythonday1new

---
+++ for loop.py
@@ -0,0 +1,5 @@
+for y in range(10):
+    if y == 8: break
+    print(y)
+else:
+    print("finished")
--- main.py
+++ main.py
@@ -4,3 +4,6 @@ fruits = ["apple", "banana", "cherry"]
 for x in adj:
     for y in fruits:
         print(x,y)
+
+
+
